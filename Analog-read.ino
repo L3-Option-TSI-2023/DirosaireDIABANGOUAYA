af7ca573b663401e97a0ecb46523b5a01a6d2b14
Dirosaire DIABANGOUAYA <127400195+DirosaireDIABANGOUAYA@users.noreply.github.com>
2023-04-26 12:16:15 +0200

    Ajout Analog-read.ino

---
+++ Analog-read.ino
@@ -0,0 +1,17 @@
+#define ANALOG_PIN 0
+#define val;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  float voltage_value = analogRead(0)*(5.0/1024);
+
+  if ()
+
+  Serial.println(voltage_value);
+}
